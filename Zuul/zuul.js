af6c57b91f9a4f27df02054e764a4de8b031bb9d
schellie <paul.schelleman@gmail.com>
2015-04-23 22:31:38 +0200

    new contents

--- Zuul/zuul.js
+++ Zuul/zuul.js
@@ -2,7 +2,7 @@
  * 
  */
 var display,
-	locations = [],
+	rooms = [],
 	items = [],
 	directions = [],
 	vocabulary = [];
@@ -11,42 +11,47 @@ var player = {
     current: 1,
     maxItems: 3,
     inventory: [],
-    move: function(nw) {
-        this.current = nw;
-    },
+    lastInput: [],
+    newInput: function(line) { this.lastInput = line.match(/\S+/g) || []; write(line, true); },
+    getVerb: function() { return vocabulary[(this.lastInput[0] || '')]; },
+    getItem: function() { return vocabulary[(this.lastInput[1] || '')]; },
+    move: function(nw) { this.current = nw; },
     loc: function() { return this.current; },
     holding: function() { return this.inventory.length; },
     toting: function(i) { return this.inventory.indexOf(i) != -1; },
-    take: function() {
-    	var i = locations[this.current].getItems();
-    	if (i.length > 0) {
-    		var ref = items.indexOf(i[0]); // get a reference for this item
-    		if (this.holding() < this.maxItems) {
-    			locations[this.current].take(i[0]); // because i[0] will be destroyed now ...
-    			this.inventory.push(items[ref]); // we push the reffered item
-    			write(items[ref].getName().toProperCase() + ' taken.');
-    		}
-    		else write('Your bag is full');
-    	} else write('There\s nothing here');
+    take: function(i) {
+    	var here = rooms[this.current].getItems();
+    	i = i || -1; // when i==0, it will be false, so -1
+    	if (!here.length) return 'There\'s nothing here'; // i is array
+    	if (this.holding() == this.maxItems) return 'Your bag is full'; // carrying the limit
+    	if (i > 0 && !rooms[this.current].here(i)) return 'No ' + items[i].getName() + ' here.';
+   		if (i <= 0 && here.length > 1) return 'Please be more specific.'; // nothing specified & more items here
+   		if (here.length == 1) i = here[0]; // point to the one item
+   		// now we can take ...
+		rooms[this.current].take(i); 
+		this.inventory.push(i); // we push the reffered item
+		return items[i].getName().toProperCase() + ' taken.';
     },
     drop: function() {
-    	if (this.holding() == 0) write('You don\'t carry anything');
+    	if (this.holding() === 0) return 'You don\'t carry anything';
     	else {
+    		var s = '';
     		for (var i in this.inventory) {
-    			locations[this.current].drop(this.inventory[i]);
-    			write(this.inventory[i].getName().toProperCase() + ' dropped.', true);
+    			//var ref = items.indexOf(this.inventory[i]); // get a reference for this item
+    			rooms[this.current].drop(this.inventory[i]);
+    			s += items[this.inventory[i]].getName().toProperCase() + ' dropped.\n';
     		}
-    		write('', false);
     		// clear inventory
     		this.inventory = [];
+    		return s;
     	}
     },
     takeInventory: function () {
     	var s = (this.inventory.length > 0) ? 'You hold:\n' : 'You don\'t hold anything\n';
         this.inventory.forEach(function (e, i) {
-            s += '- ' + e.show() + '\n';
+            s += '- ' + items[e].show() + '\n';
         });
-        write(s.slice(0, -1));
+        return s.slice(0, -1);
     }
 };
 
@@ -58,65 +63,55 @@ function write(s, noprompt) {
 	noprompt = (noprompt === 'undefined')?false:noprompt;
 	display.innerHTML += s + '\n' + (noprompt?'':'> ');
 	display.scrollTop = display.scrollHeight;
-};
+}
 
 function Act(target, cmd) {
     this.target = target;
     this.cmd = cmd;
-};
+}
 
-function Item(name, description) {
+function Item(name, description, room) {
+	room = room || 0;
 	this.name = name;
 	this.status = 0;
 	this.message = [description];
+	var index = items.length;
+	vocabulary[name] = 200 + index; // add to vocabulary
+	if (room) rooms[room].drop(index); // drop in room
 }
-Item.prototype.getName = function() {
-	return this.name;
-};
-Item.prototype.addStatus = function(m) {
-	this.message.push(m);
-};
-Item.prototype.show = function() {
-	return this.message[this.status];
-};
-Item.prototype.look = function() {
-	return 'There is ' + this.show() + ' here.';
+Item.prototype = {
+	constructor: Item,
+	getName: function() { return this.name; },
+	addStatus: function(m) { this.message.push(m); },
+	show: function() { return this.message[this.status]; },
+	look: function() { return 'There is ' + this.show() + ' here.'; },
 };
 
-
-function Location(name) {
+function Room(name) {
     this.name = name;
     this.exits = [];
     this.items = [];
 }
-Location.prototype.addExit = function(exit) {
-    this.exits.push(exit);
-};
-Location.prototype.look = function() {
-    var s = this.name + '\n';
-    this.items.forEach(function (e, i) {
-        s += e.look() + '\n';
-    });
-    s += 'Valid exits: ';
-    this.exits.forEach(function (e, i) {
-         s += directions[e.command] + ', ';
-    });
-    return s.slice(0, -2);
-};
-Location.prototype.move = function(d) {
-    for (var e in this.exits) {
-    	if (this.exits[e].command == d) return this.exits[e].target;
-    }
-    return -1;
-};
-Location.prototype.drop = function(i) {
-	this.items.push(i);
-};
-Location.prototype.take = function(i) {
-	this.items.splice(this.items.indexOf(i), 1);
-};
-Location.prototype.getItems = function() {
-	return this.items;
+Room.prototype = {
+	constructor: Room,
+	addExit: function(exit) { this.exits.push(exit); },
+	getItems: function() { return this.items; },
+	drop: function(i) { this.items.push(i); },
+	take: function(i) { this.items.splice(this.items.indexOf(i), 1); },
+	here: function(i) { return this.items.indexOf(i) >= 0; },
+	move: function(d) {
+		for (var e in this.exits) {
+			if (this.exits[e].command == d) return this.exits[e].target;
+		}
+		return -1;
+	},
+	look: function() {
+		var s = this.name + '\n';
+		this.items.forEach(function (e, i) { s += items[e].look() + '\n'; });
+		s += 'Valid exits: ';
+		this.exits.forEach(function (e, i) { s += directions[e.command] + ', '; });
+		return s.slice(0, -2);
+	}
 };
 
 function Exit(cmd, target) {
@@ -124,28 +119,28 @@ function Exit(cmd, target) {
     this.target = target;
 }
 
-function doAction(a) {
+function doAction(a, it) {
 	atype = a/100>>0;
 	switch (atype) {
 	case 0:
-	    var cl = locations[player.loc()];
+	    var cl = rooms[player.loc()];
 	    var nl = cl.move(a);
 	    if (nl == -1) write('No possible exit');
 	    else {
 	        player.move(nl);
-	        write(locations[player.loc()].look());
+	        write(rooms[player.loc()].look());
 	    }
 	    break;
 	case 1:
 		switch (a%100) {
 		case 1:
-			player.take(); break;
+			write(player.take(it%100)); break;
 		case 2:
-			player.drop(); break;
+			write(player.drop(it%100)); break;
 		case 8:
-			write(locations[player.loc()].look()); break;
+			write(rooms[player.loc()].look(it%100)); break;
 		case 9:
-			player.takeInventory(); break;
+			write(player.takeInventory()); break;
 		default:
 			write('Sorry, don\'t know how');
 		}
@@ -158,23 +153,25 @@ function doAction(a) {
 }
 
 function processCommand() {
-	var text = command.value;
-	var words = text.match(/\S+/g) || [];
-	// echo command
-	write(text, true);
+	player.newInput(command.value);
 	command.value = '';
-	//Give focus to command line.
 	command.focus();
 	
-	var action = vocabulary[words[0]];
+	var action = player.getVerb();
 	if (!action) write('I don\'t know that word');
 	else {
-		doAction(action);
+		doAction(action, player.getItem());
 	}
 }
 
+function find(name) {
+	return function (element) {
+		if (element.name == name) return true; else return false;
+	};
+}
 
 function init() {
+
 	display = document.getElementById("display");
 	command = document.getElementById("command");
     // Install command line listener
@@ -194,102 +191,69 @@ function init() {
 //	       +-v-+         
 //	       | 6 |         
 //	       +---+ 
-	locations = [
-         new Location('dead'),
-         new Location('1: Big room'),
-         new Location('2: Winding stairs'),
-         new Location('3: Cellar'),
-         new Location('4: Market square'),
-         new Location('5: West gates'),
-         new Location('6: Storage room')
-     ];
-     locations[1].addExit( new Exit(12, 2) );
-     locations[1].addExit( new Exit(16, 3) );
-     locations[2].addExit( new Exit(14, 1) );
-     locations[2].addExit( new Exit(13, 4) );
-     locations[3].addExit( new Exit(12, 4) );
-     locations[4].addExit( new Exit(14, 3) );
-     locations[4].addExit( new Exit(11, 2) );
-     locations[4].addExit( new Exit(12, 5) );
-     locations[4].addExit( new Exit(13, 6) );
-     locations[5].addExit( new Exit(14, 4) );
-     locations[6].addExit( new Exit(11, 4) );
-     
-     // 11=n, 12=e, 13=s, 14=w
-     directions[11] = 'north';
-     directions[12] = 'east';
-     directions[13] = 'south';
-     directions[14] = 'west';
-     directions[15] = 'up';
-     directions[16] = 'down';
-     
-     items = [
-         new Item('keys', 'a set of keys'),
-         new Item('axe', 'a giant axe'),
-         new Item('book', 'a dwarvish book')
-     ];
-     
-     locations[1].drop(items[0]);
-     locations[6].drop(items[1]);
-     locations[3].drop(items[2]);
-     
-     vocabulary['north'] = 11;
-     vocabulary['n'] = 11;
-     vocabulary['south'] = 13;
-     vocabulary['s'] = 13;
-     vocabulary['east'] = 12;     
-     vocabulary['e'] = 12;
-     vocabulary['west'] = 14;
-     vocabulary['w'] = 14;
-     vocabulary['up'] = 15;
-     vocabulary['u'] = 15;
-     vocabulary['down'] = 16;
-     vocabulary['d'] = 16;
+	rooms = [
+         new Room('dead'),
+         new Room('1: Big room'),
+         new Room('2: Winding stairs'),
+         new Room('3: Cellar'),
+         new Room('4: Market square'),
+         new Room('5: West gates'),
+         new Room('6: Storage room')
+    ];
+    rooms[1].addExit( new Exit(12, 2) );
+    rooms[1].addExit( new Exit(16, 3) );
+    rooms[2].addExit( new Exit(14, 1) );
+    rooms[2].addExit( new Exit(13, 4) );
+    rooms[3].addExit( new Exit(12, 4) );
+    rooms[4].addExit( new Exit(14, 3) );
+    rooms[4].addExit( new Exit(11, 2) );
+    rooms[4].addExit( new Exit(12, 5) );
+    rooms[4].addExit( new Exit(13, 6) );
+    rooms[5].addExit( new Exit(14, 4) );
+    rooms[6].addExit( new Exit(11, 4) );
+    
+    // 11=n, 12=e, 13=s, 14=w
+    directions[11] = 'north';
+    directions[12] = 'east';
+    directions[13] = 'south';
+    directions[14] = 'west';
+    directions[15] = 'up';
+    directions[16] = 'down';
+    
+    items.push( new Item('', '') );  // items 0 is not to be used (tests go wrong ...)
+    items.push( new Item('keys', 'a set of keys', 1) ); 
+    items.push( new Item('axe', 'a giant axe', 6) );
+    items.push( new Item('book', 'a dwarvish book', 3) );
+    items.push( new Item('gold', 'a bar of gold') );
 
-     vocabulary['get'] = 101;
-     vocabulary['take'] = 101;
-     vocabulary['tote'] = 101;
-     vocabulary['pick'] = 101;
-     vocabulary['drop'] = 102;
-     vocabulary['dump'] = 102;
-     vocabulary['leave'] = 102;
-     vocabulary['look'] = 108;
-     vocabulary['show'] = 108;
-     vocabulary['invent'] = 109;
-     vocabulary['inventory'] = 109;
+    vocabulary.north = 11;
+    vocabulary.n = 11;
+    vocabulary.south = 13;
+    vocabulary.s = 13;
+    vocabulary.east = 12;     
+    vocabulary.e = 12;
+    vocabulary.west = 14;
+    vocabulary.w = 14;
+    vocabulary.up = 15;
+    vocabulary.u = 15;
+    vocabulary.down = 16;
+    vocabulary.d = 16;
 
-     
-     write(locations[player.loc()].look());
- 	command.value = '';
+    vocabulary.get = 101;
+    vocabulary.take = 101;
+    vocabulary.tote = 101;
+    vocabulary.pick = 101;
+    vocabulary.drop = 102;
+    vocabulary.dump = 102;
+    vocabulary.leave = 102;
+    vocabulary.look = 108;
+    vocabulary.show = 108;
+    vocabulary.invent = 109;
+    vocabulary.inventory = 109;
+
+    write(rooms[player.loc()].look());
+	command.value = '';
 	//Give focus to command line.
 	command.focus();
 }
 
-
-/*
-// Remove an item by value in an Array object
-var arr = ['a', 'b', 'c', 'd'];
-var pos = arr.indexOf( 'c' );
-pos > -1 && arr.splice( pos, 1 );
-Print( arr ); // prints: a,b,d
-
-// Multiple-value returns
-function f() { return [1, 2]; }
-var [a, b] = f();
-Print( a + ' ' + b ); // prints: 1 2
-
-// Change current object (this) of a function call
-function test(arg) { Print( this[0]+' '+this[1]+' '+arg ); }
-var arr = ['foo', 'bar'];
-test.call(arr, 'toto'); // prints: foo bar toto
-
-// swap two variables
-var a = 1;
-var b = 2;
-[a,b] = [b,a];
-
-// Iterate on values
-for each ( var i in [3,23,4] ) Print(i)
-
-
-*/
